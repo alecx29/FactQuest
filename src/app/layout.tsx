93a6fc4a035e3c43abf0c0cc7240f737cba2f15a
Alexandru Roman <aroman@montran.com>
2025-03-14 14:08:12 +0200

    iniatialised project + added sass

---
+++ src/app/layout.tsx
@@ -0,0 +1,9 @@
+import '@/styles/global.scss';
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html>
+      <body>{children}</body>
+    </html>
+  );
+}
